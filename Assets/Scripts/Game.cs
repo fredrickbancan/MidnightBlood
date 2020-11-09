808bad9f6e9bbc8e72405482050f3a119b2f1eb5
Greywize <68831304+Greywize@users.noreply.github.com>
2020-11-09 22:00:24 +1030

    Added placeholder main menu
    Basic menu scene added
    - Start Button
    For now it sends you to the SampleScene
    - Exit Button
    Will close the application - Does not work while playing in the editor
    
    Colors and layout are all placeholder of course. Just adding the functionality.
    
    <3

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
