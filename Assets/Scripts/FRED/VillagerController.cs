847ae57d7a3fcb69f4b789c3cb018a1f892a605a
rabbet35 <rabbet35@hotmail.com>
2020-11-24 13:40:36 +1100

    Implimented villagers ability to avoid objects when walking in a certain direction
    -Logic can be applied when villager is running away from player
    -Logic can also be applied in future to Authority A.I (authority controller)

--- Assets/Scripts/FRED/VillagerController.cs
+++ Assets/Scripts/FRED/VillagerController.cs
@@ -49,21 +49,37 @@ public class VillagerController : MonoBehaviour
     public bool rotating { get => isRotating; }
     private bool isRotating = false;
 
-    public bool drawDebugFrontVector = true;
+    public bool drawDebugVectors = true;
     /// <summary>
     /// true if villager is rotating right, else rotating left.
     /// </summary>
     private bool rotRight;
+
+    /// <summary>
+    /// Direction that the villager is currently traveling in. Changes each time the villager randomly rotates, and when they are fleeing the player.
+    /// </summary>
+    private Vector3 travelDirection;
+
     // Start is called before the first frame update
     void Start()
     {
         rotRight = Random.Range(0, 2) == 0;//50% chance
         isWalkingFowards = Random.Range(0, 2) == 0;//50% chance
+        travelDirection = villagerTransform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //drawing debug frontvector
+        if(drawDebugVectors)
+        {
+            lr.positionCount = 2;
+            lr.startColor = Color.green;
+            lr.endColor = Color.black;
+            lr.SetPosition(0, villagerTransform.position);
+            lr.SetPosition(1, villagerTransform.forward * 10 + villagerTransform.position);
+        }
         handleState();
     }
 
@@ -91,7 +107,10 @@ public class VillagerController : MonoBehaviour
 
         if (isWalkingFowards)
         {
+            directAwayFromObstacles();
+
             villagerBody.velocity += villagerTransform.forward * walkSpeed;
+
             if (Random.Range(0.0F, 100.0F) <= walkStopChance)
             {
                 isWalkingFowards = false;
@@ -99,7 +118,7 @@ public class VillagerController : MonoBehaviour
         }
         else
         {
-            if(Random.Range(0.0F,100.0F) <= rotateChangeChance)
+            if(!isRotating && Random.Range(0.0F,100.0F) <= rotateChangeChance)
             {
                 rotRight = !rotRight;
             }
@@ -111,6 +130,7 @@ public class VillagerController : MonoBehaviour
                 euler.y += rotRight ? rotationAmount : -rotationAmount;
                 rot.eulerAngles = euler;
                 villagerTransform.rotation = rot;
+                travelDirection = villagerTransform.forward;
             }
         }
 
@@ -120,12 +140,38 @@ public class VillagerController : MonoBehaviour
         villagerBody.velocity = Vector3.ClampMagnitude(new Vector3(villagerBody.velocity.x, 0, villagerBody.velocity.z), maxVel);
         villagerBody.velocity = new Vector3(villagerBody.velocity.x, prevy, villagerBody.velocity.z);
 
-        //drawing debug frontvector
-        if(drawDebugFrontVector)
+    }
+
+    /// <summary>
+    /// This function is for detecting if the villager is about to walk into an obstacle, if so, it will attempt to direct the villager around it.
+    /// </summary>
+    private void directAwayFromObstacles()
+    {
+        //detect if travel detection is obstructed
+        //if so, try to rotate purpendicular to obstruction normal. (only in x,z axiees)
+        //if travel direction is not obstructed, rotate towards travel direction.
+        Ray travelRay = new Ray(villagerTransform.position, travelDirection);
+        RaycastHit info;
+        if (Physics.Raycast(travelRay, out info, 10.0F) && info.collider != this.GetComponent<Collider>())
         {
-            lr.positionCount = 2;
-            lr.SetPosition(0, villagerTransform.position);
-            lr.SetPosition(1, villagerTransform.forward + villagerTransform.position);
+            Vector3 avoidanceDir = Vector3.Normalize(new Vector3(info.normal.z,0, -info.normal.x));
+            villagerTransform.forward = Vector3.RotateTowards(villagerTransform.forward, avoidanceDir, rotationAmount * (3.14159F / 180.0F), 0.0F);
+            if (drawDebugVectors)
+            {
+                lr.startColor = Color.red;
+                lr.endColor = Color.black;
+            }
+        }
+        else
+        {
+
+            if (drawDebugVectors)
+            {
+                lr.startColor = Color.blue;
+                lr.endColor = Color.black;
+            }
+
+            villagerTransform.forward = Vector3.RotateTowards(villagerTransform.forward, travelDirection, rotationAmount * (3.14159F / 180.0F), 0.0F);
         }
     }
 }
