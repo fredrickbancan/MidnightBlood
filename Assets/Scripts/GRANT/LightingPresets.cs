8fb4b2a9ab387d1139428a3ce32e131a3b89bdd4
AntVanguard <grant.charles.bailey@gmail.com>
2020-12-01 19:00:34 +1100

    Improvements to the scene such as lighting, barriers to keep the AI and player in, roads and houses and all the rest. Will now merge with the GameManager and UI work

---
+++ Assets/Scripts/GRANT/LightingPresets.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
+
+public class LightingPresets : ScriptableObject
+{
+    // Start is called before the first frame update
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
+
+
+}
